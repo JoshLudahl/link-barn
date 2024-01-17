0cb21e0c7dd958b1dbf349ce4259b5c07ccc3f83
Josh Ludahl <josh.ludahl@gmail.com>
2024-01-16 20:43:49 -0800

    init

--- app/src/main/java/com/softklass/linkbarn/ui/theme/Color.kt
+++ app/src/main/java/com/softklass/linkbarn/ui/theme/Color.kt
@@ -9,3 +9,13 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Barn Owl Palette
+val Light = Color(240, 239, 237)
+val LightBrown = Color(169, 150, 107)
+val LightOrange = Color(221, 179, 105)
+val DarkOrange = Color(195, 139, 26)
+val DarkGray = Color(57, 53, 44)
+val Dark = Color(5, 3, 4)
+
+
