524cb02d8de7b2ee5622dd3c3c213424e550fea0
joshy <josh.ludahl@gmail.com>
2025-05-27 20:27:15 -0700

    Update code style settings and adjust theme colors
    Modified the Java code style settings to define a specific import layout. Changed the `secondaryContainerLight` color in `Color.kt` and updated the `Theme.kt` to allow for easy toggling of dynamic color theming.

--- app/src/main/java/com/softklass/linkbarn/ui/theme/Theme.kt
+++ app/src/main/java/com/softklass/linkbarn/ui/theme/Theme.kt
@@ -256,6 +256,8 @@ fun AppTheme(
     content: @Composable() () -> Unit
 ) {
   val colorScheme = when {
+
+      // Disable this part to use the default colors
       dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
           val context = LocalContext.current
           if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
