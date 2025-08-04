db3685f9c5e80cdf4e7cfd8120c258e7e27dfec9
joshy <josh.ludahl@gmail.com>
2025-08-03 23:36:31 -0700

    Refactor dismiss functionality with SwipeToDismissContainer
    Introduced a new composable `SwipeToDismissContainer` in `Dismiss.kt` to handle swipe-to-dismiss actions. This generic container uses `SwipeToDismissBox` from Material 3 to provide customizable swipe behavior for both left and right swipes.
    
    Key changes:
    - Created `SwipeToDismissContainer` which takes an item, swipe action callbacks, animation duration, content composable, and optional left/right icons.
    - Implemented `LaunchedEffect` to trigger swipe actions after an animation delay or to reset the state.
    - Utilized `AnimatedVisibility` to provide a shrinking and fading animation upon dismissal.
    - The `DismissBackground` composable is now used within `SwipeToDismissContainer` to display icons during the swipe gesture.
    - Default icons are set to `Icons.Rounded.Edit` for left swipe and `Icons.Rounded.Delete` for right swipe.

--- app/src/main/java/com/softklass/linkbarn/ui/partials/Dismiss.kt
+++ app/src/main/java/com/softklass/linkbarn/ui/partials/Dismiss.kt
@@ -1,5 +1,9 @@
 package com.softklass.linkbarn.ui.partials
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
@@ -10,15 +14,95 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Archive
 import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+
+
+@Composable
+fun <T> SwipeToDismissContainer(
+    item: T,
+    onSwipeLeft: (T) -> Unit,
+    onSwipeRight: (T) -> Unit,
+    animationDuration: Long = 500L,
+    content: @Composable (T) -> Unit,
+    leftIcon: ImageVector = Icons.Rounded.Edit,
+    rightIcon: ImageVector = Icons.Rounded.Delete,
+) {
+    val dismissState = rememberSwipeToDismissBoxState(
+        initialValue = SwipeToDismissBoxValue.Settled,
+        positionalThreshold = { distance -> distance * .25f },
+    )
+
+    var isSwipeLeftToRight by remember { mutableStateOf(false) }
+    var isSwipeRightToLeft by remember { mutableStateOf(false) }
+
+    LaunchedEffect(key1 = isSwipeLeftToRight) {
+        if (isSwipeLeftToRight) {
+            delay(animationDuration)
+            onSwipeLeft(item)
+        }
+    }
+
+    LaunchedEffect(key1 = isSwipeRightToLeft) {
+        if (isSwipeRightToLeft) {
+            onSwipeRight
+            dismissState.reset()
+            isSwipeRightToLeft = false
+        }
+    }
+
+    AnimatedVisibility(
+        visible = !isSwipeRightToLeft,
+        exit = shrinkVertically(
+            animationSpec = tween(durationMillis = 500),
+            shrinkTowards = Alignment.Top,
+        ) + fadeOut(),
+    ) {
+        SwipeToDismissBox(
+            state = dismissState,
+            enableDismissFromStartToEnd = true,
+            enableDismissFromEndToStart = true,
+            onDismiss = {
+                when (it) {
+                    SwipeToDismissBoxValue.StartToEnd -> {
+                        isSwipeLeftToRight = true
+                    }
+
+                    SwipeToDismissBoxValue.EndToStart -> {
+                        isSwipeRightToLeft = true
+                    }
+
+                    else -> {}
+                }
+            },
+            backgroundContent = {
+                DismissBackground(
+                    leftIcon = leftIcon,
+                    rightIcon = rightIcon,
+                )
+            },
+        ) {
+            content(item)
+        }
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -34,7 +118,7 @@ fun DismissBackground(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween,
 
-    ) {
+        ) {
         Icon(
             imageVector = leftIcon,
             contentDescription = "Archive",
