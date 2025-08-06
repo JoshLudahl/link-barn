5200f1d761c1366bfd94a76b350628206e7d0ac8
joshy <josh.ludahl@gmail.com>
2025-08-06 16:47:30 -0700

    Refactor FloatingActionButton in CategoriesScreen
    Updated the FloatingActionButton (FAB) in `CategoriesScreen.kt` to dynamically change based on whether categories exist.
    - If categories are present, a regular `FloatingActionButton` with an add icon is displayed.
    - If no categories exist, an `ExtendedFloatingActionButton` with an icon and "Add a category" text is shown.

--- app/src/main/java/com/softklass/linkbarn/ui/categories/CategoriesScreen.kt
+++ app/src/main/java/com/softklass/linkbarn/ui/categories/CategoriesScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -90,25 +91,27 @@ fun CategoriesScreen(
             )
         },
         floatingActionButton = {
-//            FloatingActionButton(
-//                onClick = { showAddCategoryDialog.value = true },
-//                containerColor = MaterialTheme.colorScheme.primary,
-//                shape = RoundedCornerShape(16.dp),
-//            ) {
-//                Icon(
-//                    imageVector = Icons.Rounded.Add,
-//                    contentDescription = "Add Category",
-//                    tint = MaterialTheme.colorScheme.onPrimary,
-//                )
-//            }
-
-            ExtendedFloatingActionButton(
-                onClick = { showAddCategoryDialog.value = true },
-                icon = { Icon(Icons.Rounded.Add, "Add a category", tint = MaterialTheme.colorScheme.onPrimary) },
-                text = { Text(text = "Add a category") },
-                containerColor = MaterialTheme.colorScheme.primary,
-                shape = RoundedCornerShape(16.dp),
-            )
+            if (categories.isNotEmpty()) {
+                FloatingActionButton(
+                    onClick = { showAddCategoryDialog.value = true },
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    shape = RoundedCornerShape(16.dp),
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.Add,
+                        contentDescription = "Add Category",
+                        tint = MaterialTheme.colorScheme.onPrimary,
+                    )
+                }
+            } else {
+                ExtendedFloatingActionButton(
+                    onClick = { showAddCategoryDialog.value = true },
+                    icon = { Icon(Icons.Rounded.Add, "Add a category", tint = MaterialTheme.colorScheme.onPrimary) },
+                    text = { Text(text = "Add a category") },
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    shape = RoundedCornerShape(16.dp),
+                )
+            }
         },
 
     ) { paddingValues ->
