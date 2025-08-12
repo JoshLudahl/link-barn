2c20a226cf99ea11d9f65e2cd97ae6fa14aa71f4
joshy <josh.ludahl@gmail.com>
2025-08-12 10:16:06 -0700

    Increment app version
    Updated `versionCode` to 15 and `versionName` to "2025.08.12" in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         minSdk = 26
 
         targetSdk = 36
-        versionCode = 14
-        versionName = "2025.08.06.1613"
+        versionCode = 15
+        versionName = "2025.08.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
