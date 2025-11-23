2861509c7c841d49c232c6b0b15e18ecfd1ed1a7
joshy <josh.ludahl@gmail.com>
2025-11-23 14:18:06 -0800

    Fix BottomAppBar padding

--- app/src/main/java/com/softklass/linkbarn/ui/main/MainScreen.kt
+++ app/src/main/java/com/softklass/linkbarn/ui/main/MainScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -193,6 +194,7 @@ fun EnterAlwaysTopAppBar(
         },
         bottomBar = {
             BottomAppBar(
+                modifier = Modifier.navigationBarsPadding(),
                 containerColor = MaterialTheme.colorScheme.surface,
                 actions = {
                     IconButton(onClick = { onNavigateToCategories() }) {
